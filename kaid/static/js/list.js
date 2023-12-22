3ca0bd9a0fd4ccdc04bb5a915fc85d1d32f3d28c
github-kca <mvm.datavoucher@gmail.com>
2023-12-22 18:02:34 +0900

    1222

--- kaid/static/js/list.js
+++ kaid/static/js/list.js
@@ -6,16 +6,18 @@ $(document).ready(function() {
         dataType : 'JSON',
         contentType: "application/json",
         success: function(data){
+            var col = 6
+            var screen_h = String(parseInt(window.innerHeight / col)) + "px";
             range = ((data.data.length / 6) + 1) * 6
             html = ""
             for (i=0 ; i<range ; i++) {
-                if (i % 6 == 0) html += '<div class="row mt-2">'
+                if (i % col == 0) html += '<div class="row mt-2 mb-5">'
                 if (data.data.length > i) {
-                html += '<div class="col">'
-                html += '<img src="/media'+data.data[i]['폴더경로']+'/'+data.data[i]['파일이름']+'" width="100%">'
-                html += '<span>태국 ＞ 치앙마이 ＞ ... </span>'
+                html += '<div class="col" style="height:'+screen_h+'">'
+                html += '<img src="/media'+data.data[i]['폴더경로']+'/'+data.data[i]['파일이름']+'" style="width:100%; height:100%">'
+                html += '<span>'+data.data[i]['나라']+' ＞ 여수,순천 </span>'
                 html += '</div>' } else { html += '<div class="col"></div>' }
-                if (i % 6 == 5) html += '</div>'
+                if (i % col == col-1) html += '</div>'
             }
             $('.list').html(html)
         },
@@ -26,3 +28,34 @@ $(document).ready(function() {
 
 
 })
+
+// lazy load : 스크롤 내리면 조회
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("img.lazy");    
+    var lazyloadThrottleTimeout;
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+      
+      lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageXOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+      }, 20);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  });
--- kaid/views/list_views.py
+++ kaid/views/list_views.py
@@ -4,6 +4,8 @@ from django.conf import settings
 import pandas as pd
 import os
 
+from .country_data import Country, Spot
+
 #######################################################################################################################
 # 1. MethodName : [1]list
 # 2. Comment    : 리스트 페이지 접근
@@ -11,7 +13,14 @@ import os
 # 4. 작성일      : 2023. 12. 18.
 #######################################################################################################################
 def list(request):
-	return render(request, 'list.html')
+    country_all = {'asia_list' : Country.asia, 'europe_list':Country.europe, 'america_list':Country.america,
+				'africa_list':Country.africa, 'oceania_list':Country.oceania, 'middle_east_list':Country.middle_east}
+    
+    category_all = Spot.spot_list
+
+    context = {'country' : country_all, 'category' : category_all}
+
+    return render(request, 'list.html', context)
 
 #######################################################################################################################
 # 1. MethodName : [2]getListAjax
@@ -20,8 +29,9 @@ def list(request):
 # 4. 작성일      : 2023. 12. 19.
 #######################################################################################################################
 def getListAjax(request):
-    df = pd.read_csv(f'{os.getcwd()}/kaid/static/data/sample.csv', encoding='utf-8-sig', dtype=object).fillna('')
+    df = pd.read_csv(f'{os.getcwd()}/kaid/static/data/sample_kr.csv', encoding='utf-8-sig', dtype=object).fillna('')
     res = {}
     data = df.to_dict('records')
     res['data'] = data
+
     return JsonResponse(res)
