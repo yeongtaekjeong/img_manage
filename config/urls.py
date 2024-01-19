956b02bb8aefe57162c7e002c9f89ca110012d31
github-kca <mvm.datavoucher@gmail.com>
2024-01-19 17:06:44 +0900

    0119

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
     path('getDetailAjax/', list_views.getDetailAjax),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# urlpatterns.append(url(r'^media/(?P<path>.*)/$', serve, kwargs={'insecure': True}))
+urlpatterns.append(url(r'^media/(?P<path>.*)/$', serve, kwargs={'insecure': True}))
 # urlpatterns.append(url(r'^static/(?P<path>.*)/$', serve, kwargs={'insecure': True}))
