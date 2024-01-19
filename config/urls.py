94f913b433ea0d67c5aea3c38dc007f0a08819d1
github-kca <mvm.datavoucher@gmail.com>
2024-01-19 17:08:31 +0900

    0119

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns.append(url(r'^media/(?P<path>.*)/$', serve, kwargs={'insecure': True}))
+# urlpatterns.append(url(r'^media/(?P<path>.*)/$', serve, kwargs={'insecure': True}))
 # urlpatterns.append(url(r'^static/(?P<path>.*)/$', serve, kwargs={'insecure': True}))
+urlpatterns.append(url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}))
