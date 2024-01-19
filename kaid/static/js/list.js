f9246e4df311d2b8de03dc3a98263168e2f62bff
github-kca <mvm.datavoucher@gmail.com>
2024-01-19 16:44:43 +0900

    0119

--- kaid/static/js/list.js
+++ kaid/static/js/list.js
@@ -43,6 +43,7 @@ function paging_set(num) {
           }
           if (count > i) {
             var img_path = String(data.data[i]['img_path_y'])
+            console.log(img_path)
             html += '<div class="col-sm mb-2" id="listimg" style="height:'+screen_h+'; cursor:pointer">'
             html += '<img src="/media/'+img_path+'" onclick="detail_img(this)"'+
                     'onerror="this.onerror=null; this.src=\'../static/img/noimg.png\';" style="width:100%; height:82%">'
